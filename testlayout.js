c08ae07ca9044dd3e2655fa8d48b9375e4faaa54
Blake Engineering Club <dkhan@blakeschool.org>
2026-04-26 17:16:40 -0500

    Update testlayout.js

--- testlayout.js
+++ testlayout.js
@@ -9,7 +9,10 @@ const URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:
 
 let questions = [];
 let currentQuestionIndex = 0;
-let score = 0;
+let reds = 0;
+let blues = 0;
+let greens = 0;
+let yellows = 0;
 
 const clues = [];
 const question = document.getElementById('questionBox');
@@ -23,6 +26,63 @@ const greenMinus = document.getElementById('greenMinus');
 const greenPlus = document.getElementById('greenPlus');
 const yellowMinus = document.getElementById('yellowMinus');
 const yellowPlus = document.getElementById('yellowPlus');
+const redScore = document.getElementById('redScore');
+const blueScore = document.getElementById('blueScore');
+const greenScore = document.getElementById('greenScore');
+const yellowScore = document.getElementById('yellowScore');
+
+fetchQuestions();
+
+redMinus.addEventListener('click',rmclick);
+redPlus.addEventListener('click',rpclick);
+blueMinus.addEventListener('click',bmclick);
+bluePlus.addEventListener('click',bpclick);
+greenMinus.addEventListener('click',gmclick);
+greenPlus.addEventListener('click',gpclick);
+yellowMinus.addEventListener('click',ymclick);
+yellowPlus.addEventListener('click',ypclick);
+
+function rmclick(){
+    reds -= 100;
+    redScore.innerText = reds;
+}
+
+function rpclick(){
+    reds += 100;
+    redScore.innerText = reds;
+}
+
+function bmclick(){
+    blues -= 100;
+    blueScore.innerText = blue;
+}
+
+function bpclick(){
+    blues += 100;
+    blueScore.innerText = blues;
+}
+
+function gmclick(){
+    greens -= 100;
+    greenScore.innerText = greens;
+}
+
+function gpclick(){
+    greens += 100;
+    greenScore.innerText = greens;
+}
+
+function ymclick(){
+    yellows -= 100;
+    yellowScore.innerText = yellows;
+}
+
+function ypclick(){
+    yellows += 100;
+    yellowScore.innerText = yellows;
+}
+
+
 
 question.addEventListener('click', (event) => {
     main.classList.remove('is-flipped');
@@ -41,7 +101,7 @@ async function fetchQuestions() {
         console.error("Error fetching data:", error);
     }
 }
-fetchQuestions();
+
 console.log(questions);
 let cat = ["questionA","questionB","questionC","questionD","questionE","questionF"];
 for (let i = 0; i < 6; i++) {
