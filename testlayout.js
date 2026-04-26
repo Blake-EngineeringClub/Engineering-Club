ef7dab19a636f464d61f2522f3dc34e59fa1fd66
Blake Engineering Club <dkhan@blakeschool.org>
2026-04-25 21:27:42 -0500

    Update testlayout.js

--- testlayout.js
+++ testlayout.js
@@ -2,3 +2,23 @@
 setTimeout(function() {
     document.getElementById('overlay').remove();
 }, 3000);
+const clues = [];
+const question = document.getElementById('question');
+const qtitle = document.getElementById('qtitle');
+
+let cat = ["A","B","C","D","E","F"];
+
+for (let i = 0; i < 7; i++) {
+    clues[i]=[];
+    for (let j = 0; j < 6; j++) {
+        let clueId = cat[i]+j;
+        let x = this.myListener.bind(this,i,j);
+        clues[i][j]=document.getElementById(clueId);
+        clues[i][j].addEventListner('click',x);
+    }
+}
+
+function myListener(x,y){
+    question.innerText = x;
+    qtitle.innerText = y
+}
